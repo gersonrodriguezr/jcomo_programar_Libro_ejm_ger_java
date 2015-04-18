5ef5a520a85c3b88c9f31e98e02a0f362dabb767
gersonrodriguezr <gersonrodriguezr@gmail.com>
2015-04-18 13:47:01 -0500

    Suando dos numeros usando la clase Scanner

---
+++ capitulo 2/Fig2_1_Bienvenido1/src/suma/Suma1.java
@@ -0,0 +1,36 @@
+/*
+ *Muestra la suma de dos numeros
+ *Usa el objeto "Scanner"
+ */
+package suma;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gerson
+ */
+public class Suma1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        int numero1, numero2, numero3, suma;
+        
+        System.out.println("Escriba el primer entero:");
+        numero1 = entrada.nextInt();
+        
+        System.out.println("\nEscriba el segundo numero:");
+        numero2 = entrada.nextInt();
+        
+        suma = numero1 + numero2;
+        
+        System.out.printf("La suma es:%d\n",suma);
+        
+        
+    }
+    
+}
