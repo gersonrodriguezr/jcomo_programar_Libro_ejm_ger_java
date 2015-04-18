0c599d056dca8ac1e99548f5f2e4cc978e0e8eb5
gersonrodriguezr <gersonrodriguezr@gmail.com>
2015-04-18 12:22:05 -0500

    Uso de caracteres especiales \n

---
+++ capitulo 2/Fig2_1_Bienvenido1/src/fig2_1_bienvenido1/Fig2_4_Bienvenido3.java
@@ -0,0 +1,19 @@
+/*
+ * Uso de caracteres de nueva linea
+ */
+package fig2_1_bienvenido1;
+
+/**
+ *
+ * @author gerson
+ */
+public class Fig2_4_Bienvenido3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos\na la\nprogramación\nen Java");
+    }
+    
+}
