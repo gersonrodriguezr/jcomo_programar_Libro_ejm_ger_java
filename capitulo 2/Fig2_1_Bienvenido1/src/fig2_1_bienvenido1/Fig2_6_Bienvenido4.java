ae84da79e4abc0ff677108cd986751f5f13e3a16
gersonrodriguezr <gersonrodriguezr@gmail.com>
2015-04-18 13:05:44 -0500

    usando prinftf, es decir formato

---
+++ capitulo 2/Fig2_1_Bienvenido1/src/fig2_1_bienvenido1/Fig2_6_Bienvenido4.java
@@ -0,0 +1,19 @@
+/*
+ * uso del printf
+ */
+package fig2_1_bienvenido1;
+
+/**
+ *
+ * @author gerson
+ */
+public class Fig2_6_Bienvenido4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("%s\n%s\n","Bienvenido","A la programación en Java");
+    }
+    
+}
